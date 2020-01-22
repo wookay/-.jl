b2a6f7a4d9b7babff8261f83d81c813fb14101b4
WooKyoung Noh <wookay.noh@gmail.com>
2020-01-22 13:25:59 +0900

    mapo

---
+++ test/guo/mapo.jl
@@ -0,0 +1,10 @@
+module test_guo_mapo
+
+using Test
+using Guo
+
+@testset "마포대교" begin
+    @test 마포대교 == "마포대교는 무너졌냐 이새끼야"
+end
+
+end # module test_guo_mapo
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,2 @@
-using Test
-using Guo
-
-@testset "마포대교" begin
-    @test 마포대교 ==  "마포대교는 무너졌냐 이새끼야"
-end
+using Jive
+runtests(@__DIR__)
